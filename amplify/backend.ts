83e30b7d144b388ad75c49949dbe6248c544bf10
takeshi_honda <honda0924@hotmail.co.jp>
2025-06-06 23:08:52 +0900

    update: 一旦postConfirmationのことは忘れる

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,10 +1,10 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
-import { postConfirmationFunction } from './functions/postConfirmation/resource';
+// import { postConfirmationFunction } from './functions/postConfirmation/resource';
 
 export const backend = defineBackend({
   auth,
   data,
-  postConfirmationFunction,
+  // postConfirmationFunction,
 });
